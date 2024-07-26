5cccf2a2126a10c4fcd3df373110b5da82358751
Riggs <xconnorx@gmx.de>
2024-07-26 10:39:28 +0200

    Update sh_item.lua

--- gamemode/core/libs/sh_item.lua
+++ gamemode/core/libs/sh_item.lua
@@ -166,7 +166,8 @@ function ix.item.Register(uniqueID, baseID, isBaseItem, path, luaGenerated)
                         client:NotifyLocalized(error or "unknownError")
                         return false
                     else
-                        client:EmitSound("npc/zombie/foot_slide" .. math.random(1, 3) .. ".wav", 75, math.random(90, 120), 1)
+                        local path, level, pitch, volume = hook.Run("GetItemPickupSound", item) or "npc/zombie/foot_slide" .. math.random(1, 3) .. ".wav", 75, math.random(90, 120), 1
+                        client:EmitSound(path, level, pitch, volume)
 
                         if (item.data) then // I don't like it but, meh...
                             for k, v in pairs(item.data) do
