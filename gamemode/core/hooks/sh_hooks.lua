ec522932659a915776ccbb613c7c392b6a27053f
Riggs <xconnorx@gmx.de>
2024-08-13 19:53:39 +0200

    Update sh_hooks.lua

--- gamemode/core/hooks/sh_hooks.lua
+++ gamemode/core/hooks/sh_hooks.lua
@@ -171,24 +171,23 @@ function GM:DoAnimationEvent(client, event, data)
     if (IsValid(weapon)) then
         local animation = client.ixAnimTable
 
+        local attack = isstring(animation.attack) and client:LookupSequence(animation.attack) or animation.attack or ACT_GESTURE_RANGE_ATTACK_SMG1
+        local reload = isstring(animation.reload) and client:LookupSequence(animation.reload) or animation.reload or ACT_GESTURE_RELOAD_SMG1
+
         if (event == PLAYERANIMEVENT_ATTACK_PRIMARY) then
-            client:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, animation.attack or ACT_GESTURE_RANGE_ATTACK_SMG1, true)
+            client:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, attack, true)
 
             return ACT_VM_PRIMARYATTACK
         elseif (event == PLAYERANIMEVENT_ATTACK_SECONDARY) then
-            client:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, animation.attack or ACT_GESTURE_RANGE_ATTACK_SMG1, true)
+            client:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, attack, true)
 
             return ACT_VM_SECONDARYATTACK
         elseif (event == PLAYERANIMEVENT_RELOAD) then
-            client:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, animation.reload or ACT_GESTURE_RELOAD_SMG1, true)
+            client:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, reload, true)
 
             return ACT_INVALID
         elseif (event == PLAYERANIMEVENT_JUMP) then
             client:AnimRestartMainSequence()
-
-            return ACT_INVALID
-        elseif (event == PLAYERANIMEVENT_CANCEL_RELOAD) then
-            client:AnimResetGestureSlot(GESTURE_SLOT_ATTACK_AND_RELOAD)
 
             return ACT_INVALID
         end
